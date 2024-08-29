0cb8a2291e5727af9aae35a7c746a6eecc2437cb
radhika293 <154504538+radhika293@users.noreply.github.com>
2024-08-29 11:45:24 +0530

    Add files via upload

---
+++ Java/Activities/Car.java
@@ -0,0 +1,29 @@
+public class Car {
+    String color;
+    int make;
+    String transmission;
+    int doors;
+    int tyres;
+
+    Car() {
+        doors = 4;
+        tyres = 4;
+    }
+
+    public void displayCharacteristics(){
+        System.out.println("Color of the Car:" + color);
+	System.out.println("Make of the Car:" + make); 
+	System.out.println("Transmission of the Car:" + transmission);
+	System.out.println("Number of doors:" + doors); 
+	System.out.println("Number of tyres:" + tyres);
+    }
+
+    public void accelerate() {
+        System.out.println("Car is moving forward.");
+    }
+    
+    public void brake() {
+        System.out.println("Car has stopped.");
+    }
+}
+	   
